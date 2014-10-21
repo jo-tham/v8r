5e777b70650f461a0b5acf288398e2a65fc0db67
Jotham Apaloo <jothamapaloo@gmail.com>
2014-10-20 20:33:49 -0400

    Add a test for another input type.

--- tests/testthat/test-session-results.R
+++ tests/testthat/test-session-results.R
@@ -4,4 +4,5 @@ test_that("session_results returns data frame", {
     session_url <- "http://www.v8supercars.com.au/championship/2014-v8-supercars-championship/supercheap-auto-bathurst-1000/sessions/championship/qualifying"
     expect_is(session_results(session_url), "data.frame")
     expect_error(session_results())
+    expect_error(session_results(1))
 })
