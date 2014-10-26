61a1c111671b90122655575e425c480051ed874d
Jotham Apaloo <jothamapaloo@gmail.com>
2014-10-26 18:52:04 -0400

    Move todo items to github issues.

--- R/session-results.R
+++ R/session-results.R
@@ -1,9 +1,5 @@
 ##' Parse session results from webpage
 ##'
-##' Should add some means of trying to cleverly distinguish
-##' results tables from other tables
-##' could add class for the series, i.e. v8sc, f1, motogp, wrc
-##' 
 ##' @title session_results
 ##' @param session_url character, link to v8supercars.com.au
 ##' results page
